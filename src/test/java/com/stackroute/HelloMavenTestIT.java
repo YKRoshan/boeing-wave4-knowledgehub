bb2374c96fd58978597704dc3a69fd066e3ab871
Meghna <meghna@stackroute.in>
2019-02-04 20:10:51 +0530

    updated config and pom.xml

--- src/test/java/com/stackroute/HelloMavenTestIT.java
+++ src/test/java/com/stackroute/HelloMavenTestIT.java
@@ -4,6 +4,11 @@ import org.junit.Test;
 
 public class HelloMavenTestIT {
 
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
     @Test
     public void testGreetUserIT() {
         System.out.println("Integration test running...");
