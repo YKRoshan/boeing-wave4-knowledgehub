477b3a1353a241d6ef825e9aba5577695dec8267
santhoshtn <santhoshtn1997@gmail.com>
2019-02-14 17:07:39 +0530

    Added Login-service Backend

---
+++ login-service/src/main/java/com/stackroute/knowledgehub/loginservice/controller/AuthRestAPI.java
@@ -0,0 +1,50 @@
+package com.stackroute.knowledgehub.loginservice.controller;
+
+
+import com.stackroute.knowledgehub.loginservice.message.request.LoginForm;
+import com.stackroute.knowledgehub.loginservice.message.response.JwtResponse;
+import com.stackroute.knowledgehub.loginservice.repository.UserRepository;
+import com.stackroute.knowledgehub.loginservice.security.jwt.JwtProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthRestAPI {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginForm loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateJwtToken(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getUsername()));
+    }
+
+}
