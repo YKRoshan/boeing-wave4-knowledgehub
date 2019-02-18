35e26ed332dfaac84d776d5301e81238f3074855
venkatanagasairam.sakhamuri <venkatanagasairam.sakhamuri@boeing.com>
2019-02-18 12:40:40 +0530

    Made changes to the methods present in NLPServiceImpl.java class

--- analytic-server/src/main/java/com/stackroute/AnalyticServerApplication.java
+++ analytic-server/src/main/java/com/stackroute/AnalyticServerApplication.java
@@ -10,13 +10,11 @@ public class AnalyticServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AnalyticServerApplication.class, args);
         NlpServiceImpl nlpService = new NlpServiceImpl();
-        nlpService.setParagraph("The Spring Framework Inversion of Control (IoC) component addresses this concern by providing a formalized means of composing disparate components into a fully working application ready for use. The Spring Framework codifies formalized design patterns as first-class objects that you can integrate into your own application(s). Numerous organizations and institutions use the Spring Framework in this manner to engineer robust, maintainable applications.");
-        nlpService.getAllWords();
-        nlpService.getWordsWithoutStopWords();
-        nlpService.frequencyOfWords();
-        nlpService.getAllTokens();
-        nlpService.getStemmedWords();
-        nlpService.getAllSentences();
-        nlpService.getLemmitizedWords();
+        nlpService.setParagraph("The Spring Framework Inversion of Control (IoC) component " +
+                "addresses this concern by providing a formalized means of composing disparate components into a" +
+                " fully working application ready for use. The Spring Framework codifies formalized design patterns as" +
+                " first-class objects that you can integrate into your own application(s). Numerous organizations and institutions" +
+                " use the Spring Framework in this manner to engineer robust, maintainable applications.");
+        nlpService.showAllResults();
     }
 }
--- analytic-server/src/main/java/com/stackroute/NlpService/NlpServiceImpl.java
+++ analytic-server/src/main/java/com/stackroute/NlpService/NlpServiceImpl.java
@@ -6,9 +6,9 @@ import com.aliasi.tokenizer.*;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
-import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.tokenize.SimpleTokenizer;
 
 import java.io.*;
@@ -50,17 +50,29 @@ public class NlpServiceImpl {
         this.paragraph = paragraph;
     }
 
-    public ArrayList<String> getAllSentences() {
+    public String getCleanerParagrah() {
+        String inputParagraph = this.paragraph;
+        // Data Cleaning by removing extra spaces.
+        inputParagraph = inputParagraph.trim();
+        inputParagraph = inputParagraph.replaceAll("\\s+", " ");
+        inputParagraph = inputParagraph.replaceAll("\\t", " ");
+
+        String[] tokenizedWord = inputParagraph.split(" ");
+        StringBuffer cleanedParagraph = new StringBuffer();
+        for (int i = 0; i < tokenizedWord.length; i++) {
+            cleanedParagraph.append(tokenizedWord[i] + " ");
+        }
+        return cleanedParagraph.toString().trim();
+    }
+
+    public ArrayList<String> getAllTokenizedSentences() {
         ArrayList<String> tokenizedSentences = new ArrayList<>();
         TokenizerFactory tokenizerFactory = IndoEuropeanTokenizerFactory.INSTANCE;
         SentenceModel sentenceModel = new IndoEuropeanSentenceModel();
         List<String> tokenList = new ArrayList<>();
         List<String> whiteSpaceList = new ArrayList<>();
-        Tokenizer tokenizer = tokenizerFactory.tokenizer(this.paragraph.toCharArray(), 0, this.paragraph.length());
+        Tokenizer tokenizer = tokenizerFactory.tokenizer(getCleanerParagrah().toCharArray(), 0, getCleanerParagrah().length());
         tokenizer.tokenize(tokenList, whiteSpaceList);
-        System.out.println("token List");
-        System.out.println(tokenList);
-        System.out.println(whiteSpaceList);
         String tokens[] = new String[tokenList.size()];
         String whiteSpaces[] = new String[whiteSpaceList.size()];
         tokenList.toArray(tokens);
@@ -75,111 +87,20 @@ public class NlpServiceImpl {
                 start++;
             }
             tokenizedSentences.add(temporaryString);
-            System.out.println(temporaryString);
-            System.out.println(tokenizedSentences);
         }
-        System.out.println("tokenized Sentences");
-        System.out.println(tokenizedSentences);
         return tokenizedSentences;
     }
 
-    public ArrayList<String> getAllWords() {
-        String inputParagraph = getCleanerParagrah();
-        String[] tokenizedWord = inputParagraph.split(" ");
-        ArrayList<String> tokenizedWordList = new ArrayList<>();
-        for (int i = 0; i < tokenizedWord.length; i++) {
-            tokenizedWordList.add(tokenizedWord[i]);
+    public String getParagrahWithSentences() {
+        ArrayList<String> sentences = new ArrayList<>(getAllTokenizedSentences());
+        StringBuffer sentencedParagraph = new StringBuffer();
+        for (int i = 0; i < sentences.size(); i++) {
+            sentencedParagraph.append(sentences.get(i) + " ");
         }
-        System.out.println(tokenizedWordList);
-        return tokenizedWordList;
-    }
-
-    public String getCleanerParagrah() {
-        String inputParagraph = this.paragraph;
-        // Data Cleaning by removing extra spaces.
-        inputParagraph = inputParagraph.toLowerCase();
-        inputParagraph = inputParagraph.trim();
-        inputParagraph = inputParagraph.replaceAll("\\s+", " ");
-        inputParagraph = inputParagraph.replaceAll("\\t", " ");
-
-        String[] tokenizedWord = inputParagraph.split(" ");
-        StringBuffer cleanedParagraph = new StringBuffer();
-        for (int i = 0; i < tokenizedWord.length; i++) {
-            cleanedParagraph.append(tokenizedWord[i] + " ");
-        }
-        return cleanedParagraph.toString().trim();
-    }
-
-    public ArrayList<String> getAllTokens() {
-        SimpleTokenizer simpleTokenizer = SimpleTokenizer.INSTANCE;
-        String[] tokens = simpleTokenizer.tokenize(getCleanerParagrah());
-        ArrayList<String> tokensList = new ArrayList<>();
-        for (int i = 0; i < tokens.length; i++) {
-            tokensList.add(tokens[i]);
-        }
-        System.out.println(tokensList);
-        return tokensList;
-    }
-
-    public ArrayList<String> getWordsWithoutStopWords() {
-        ArrayList<String> wordsWithOutStopwords = getAllTokens();
-        for (int j = 0; j < stopwords.length; j++) {
-            if (wordsWithOutStopwords.contains(stopwords[j])) {
-                wordsWithOutStopwords.remove(stopwords[j]);//remove it
-            }
-        }
-        System.out.println(wordsWithOutStopwords);
-        return wordsWithOutStopwords;
-    }
-
-    public String getParagraphWithOutStopWords() {
-        ArrayList<String> wordsWithOutStopwords = getAllTokens();
-        for (int j = 0; j < stopwords.length; j++) {
-            if (wordsWithOutStopwords.contains(stopwords[j])) {
-                wordsWithOutStopwords.remove(stopwords[j]);//remove it
-            }
-        }
-        StringBuffer paragraphWithOutStopWords = new StringBuffer();
-        for (int i = 0; i < wordsWithOutStopwords.size(); i++) {
-            paragraphWithOutStopWords.append(wordsWithOutStopwords.get(i) + " ");
-        }
-        return paragraphWithOutStopWords.toString().trim();
-    }
-
-    public HashMap<String, Long> frequencyOfWords() {
-        ArrayList<String> wordsWithOutStopwords = getWordsWithoutStopWords();
-        HashMap<String, Long> wordsFrequencyMap = new HashMap<>();
-        for (int i = 0; i < wordsWithOutStopwords.size(); i++) {
-            if (wordsFrequencyMap.containsKey(wordsWithOutStopwords.get(i))) {
-                long count = wordsFrequencyMap.get(wordsWithOutStopwords.get(i));
-                count++;
-                wordsFrequencyMap.put(wordsWithOutStopwords.get(i), count);
-            } else {
-                long one = 1;
-                wordsFrequencyMap.put(wordsWithOutStopwords.get(i), one);
-            }
-        }
-        System.out.println(wordsFrequencyMap);
-        return wordsFrequencyMap;
-    }
-
-    public List<String> getStemmedWords() {
-        TokenizerFactory tokenizerFactory = IndoEuropeanTokenizerFactory.INSTANCE;
-        TokenizerFactory porterFactory = new PorterStemmerTokenizerFactory(tokenizerFactory);
-        ArrayList<String> wordTokens = getWordsWithoutStopWords();
-        List<String> stemmedWordsList = new ArrayList<>();
-        for (String word : wordTokens) {
-            Tokenization tokenization = new Tokenization(word, porterFactory);
-            stemmedWordsList.add(tokenization.tokenList().toString());
-        }
-        System.out.println("get stemmed words");
-        System.out.println(stemmedWordsList);
-        return stemmedWordsList;
+        return sentencedParagraph.toString().trim();
     }
 
     public ArrayList<String> getLemmitizedWords() {
-        System.out.println("Lemmitization");
-
         Properties properties = new Properties();
         properties.setProperty("annotator", "lemma");
         // StanfordCoreNLP uses pipeline and this pipeline is create
@@ -188,7 +109,7 @@ public class NlpServiceImpl {
         StanfordCoreNLP pipeline = new StanfordCoreNLP(properties);
         // This annotations object gives the special meaning to the
         // string we used in propeties.put() method
-        Annotation annotations = new Annotation(getParagraphWithOutStopWords());
+        Annotation annotations = new Annotation(getParagrahWithSentences());
         // pipeline.annotate(annotations)  provies the annotation to those particular objects.
         pipeline.annotate(annotations);
         // sentenceList contains list of sentences
@@ -201,10 +122,99 @@ public class NlpServiceImpl {
                 originalWords.add(word.originalText());
             }
         }
-        System.out.println(originalWords);
-        System.out.println(lemmaWords);
         return lemmaWords;
     }
 
+    public List<String> getStemmedWords() {
+        TokenizerFactory tokenizerFactory = IndoEuropeanTokenizerFactory.INSTANCE;
+        TokenizerFactory porterFactory = new PorterStemmerTokenizerFactory(tokenizerFactory);
+        ArrayList<String> wordTokens = getLemmitizedWords();
+        List<String> stemmedWordsList = new ArrayList<>();
+        for (String word : wordTokens) {
+            Tokenization tokenization = new Tokenization(word, porterFactory);
+            stemmedWordsList.add(tokenization.tokenList().toString());
+        }
+        return stemmedWordsList;
+    }
+
+    public ArrayList<String> getWordsWithoutStopWords() {
+        ArrayList<String> wordsWithOutStopwords = getLemmitizedWords();
+        for (int j = 0; j < stopwords.length; j++) {
+            if (wordsWithOutStopwords.contains(stopwords[j])) {
+                wordsWithOutStopwords.remove(stopwords[j]);//remove it
+            }
+        }
+        return wordsWithOutStopwords;
+    }
+
+    public String getParagraphWithOutStopWords() {
+        ArrayList<String> wordsWithOutStopwords = getWordsWithoutStopWords();
+        StringBuffer paragraphWithOutStopWords = new StringBuffer();
+        for (int i = 0; i < wordsWithOutStopwords.size(); i++) {
+            paragraphWithOutStopWords.append(wordsWithOutStopwords.get(i) + " ");
+        }
+        return paragraphWithOutStopWords.toString().trim();
+    }
+
+    public ArrayList<POSTagging> getPOSWords() {
+        Properties properties = new Properties();
+        properties.setProperty("annotator", "pos");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(properties);
+        CoreDocument coreDocument = new CoreDocument(getParagraphWithOutStopWords());
+        pipeline.annotate(coreDocument);
+        List<CoreLabel> coreLabelsList = coreDocument.tokens();
+        ArrayList<POSTagging> wordsWithPOSTag = new ArrayList<>();
+        for (CoreLabel coreLabel : coreLabelsList) {
+            String partsOfSpeech = coreLabel.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+            wordsWithPOSTag.add(new POSTagging(coreLabel.originalText(), partsOfSpeech));
+        }
+        return wordsWithPOSTag;
+    }
+
+    public HashMap<String, Long> getFrequencyOfWords() {
+        ArrayList<String> wordsWithOutStopwords = getWordsWithoutStopWords();
+        HashMap<String, Long> wordsFrequencyMap = new HashMap<>();
+        for (int i = 0; i < wordsWithOutStopwords.size(); i++) {
+            if (wordsFrequencyMap.containsKey(wordsWithOutStopwords.get(i))) {
+                long count = wordsFrequencyMap.get(wordsWithOutStopwords.get(i));
+                count++;
+                wordsFrequencyMap.put(wordsWithOutStopwords.get(i), count);
+            } else {
+                long one = 1;
+                wordsFrequencyMap.put(wordsWithOutStopwords.get(i), one);
+            }
+        }
+        return wordsFrequencyMap;
+    }
+
+    public void showAllResults() {
+        System.out.println("Get Cleared Paragraph");
+        String clearedParagraph = new String(getCleanerParagrah());
+        System.out.println(clearedParagraph);
+
+        System.out.println("Get all tokened sentences");
+        ArrayList<String> allSentences = new ArrayList<>(getAllTokenizedSentences());
+        System.out.println(allSentences);
+
+        System.out.println("Lemmitization");
+        ArrayList<String> allLemmas = new ArrayList<>(getLemmitizedWords());
+        System.out.println(allLemmas);
+
+        System.out.println("Stemming");
+        ArrayList<String> allStems = new ArrayList<>(getStemmedWords());
+        System.out.println(allStems);
+
+        System.out.println("Stop Word Removal");
+        ArrayList<String> allStopWords = new ArrayList<>(getWordsWithoutStopWords());
+        System.out.println(allStopWords);
+
+        System.out.println("Stop Word Removal Paragraph");
+        String paragraphWithOutStopWords = new String(getParagraphWithOutStopWords());
+        System.out.println(paragraphWithOutStopWords);
+
+        System.out.println("POS TAGGING");
+        ArrayList<POSTagging> posTaggings = new ArrayList<>(getPOSWords());
+        System.out.println(posTaggings);
+    }
 
 }
