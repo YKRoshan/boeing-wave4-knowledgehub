90d838da08eef685872f3e6d80744f20bbbee84c
santhoshtn <santhoshtn1997@gmail.com>
2019-02-20 12:26:29 +0530

    added proper comments

--- login-service/src/main/java/com/stackroute/controller/TestRestAPI.java
+++ login-service/src/main/java/com/stackroute/controller/TestRestAPI.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-//this controller class is used for testing the authentication
+//To test the authentication
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
