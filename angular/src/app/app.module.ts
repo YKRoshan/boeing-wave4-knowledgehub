11cbfafc53a2491a92653545494a3ca1f297d102
gauravgkp <gkpgauravsingh@gmail.com>
2019-02-21 09:38:52 +0530

    angular initial commit

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,10 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
-
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
@@ -12,16 +8,25 @@ import { HomeComponent } from './home/home.component';
 import { TokenService } from './service/token.service';
 import { AuthService } from './service/auth.service';
 import { HttpClientModule } from '@angular/common/http';
-
-
-
+import { UploaderComponent } from './uploader/uploader.component';
+import { UploadService } from './upload.service';
+import { FileDropModule } from 'ngx-file-drop';
+import { LoginbuttonComponent } from './loginbutton/loginbutton.component';
+import { SearchbuttonComponent } from './searchbutton/searchbutton.component';
+import { FooterComponent } from './footer/footer.component';
+import {MaterialModule} from './material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-
+    UploaderComponent,
+    LoginbuttonComponent,
+    SearchbuttonComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -34,11 +39,16 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
-   
-    //FormBuilder,
+    // FileDropModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserTransferStateModule,
+    BrowserAnimationsModule
   ],
   providers: [TokenService,
-              AuthService],
+              AuthService,
+              UploadService],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [AppComponent]
 })
