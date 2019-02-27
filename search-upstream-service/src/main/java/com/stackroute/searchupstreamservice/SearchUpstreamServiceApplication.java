3dae7efa63b49171bd70e683ecf3849bb7049815
santhoshtn <santhoshtn1997@gmail.com>
2019-02-27 19:57:36 +0530

    added search upstream service

---
+++ search-upstream-service/src/main/java/com/stackroute/searchupstreamservice/SearchUpstreamServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.searchupstreamservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchUpstreamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchUpstreamServiceApplication.class, args);
+	}
+
+}
