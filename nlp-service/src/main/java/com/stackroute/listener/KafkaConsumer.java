6169fa2f3e35afd03171dc37707efaa97e3e66ea
yasawant.k.rosan <yasawant.k.rosan@boeing.com>
2019-02-28 19:58:16 +0530

    Added commented code for NLP service for Kafka

--- nlp-service/src/main/java/com/stackroute/listener/KafkaConsumer.java
+++ nlp-service/src/main/java/com/stackroute/listener/KafkaConsumer.java
@@ -1,50 +1,50 @@
-package com.stackroute.listener;
-
-import com.stackroute.domain.NlpResult;
-import com.stackroute.domain.SearchJSON;
-import com.stackroute.service.NlpService;
-import com.stackroute.service.QuestionStorageService;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-
-@Service
-public class KafkaConsumer {
-    private KafkaProducer kafkaProducer;
-    private NlpService nlpService;
-    private QuestionStorageService questionStorageService;
-
-    @Autowired
-    public KafkaConsumer(NlpService nlpService,
-                         KafkaProducer kafkaProducer,
-                         QuestionStorageService questionStorageService) {
-        this.kafkaProducer = kafkaProducer;
-        this.nlpService = nlpService;
-        this.questionStorageService = questionStorageService;
-    }
-
-    @KafkaListener(topics = "UpstreamResults", groupId = "group_id")
-    public void consume(String message) {
-        JSONObject object = (JSONObject) JSONValue.parse(message);
-        SearchJSON searchJSON = new SearchJSON(object.get("searchString").toString(), object.get("sessionId").toString());
-        ArrayList<String> concepts = new ArrayList<>(Arrays.asList("annotations", "ioc container", "beans", "spring core", "spring data jpa", "spring datajpa", "spring aop", "spring security", "spring cloud", "spring reactive", "spring mvc"));
-        nlpService.setSessonId(searchJSON.getSessonId());
-        nlpService.setParagraph(searchJSON.getSearchString());
-        nlpService.setConceptName(concepts);
-        NlpResult nlpResult = nlpService.getNlpResults();
-        if (nlpResult.getIntent().equalsIgnoreCase("no intent found")) {
-            String searchStringQuestion = searchJSON.getSearchString();
-            questionStorageService.saveQuestion(searchStringQuestion);
-            kafkaProducer.postservice();
-        } else {
-            kafkaProducer.postservice();
-        }
-    }
-}
-
+//package com.stackroute.listener;
+//
+//import com.stackroute.domain.NlpResult;
+//import com.stackroute.domain.SearchJSON;
+//import com.stackroute.service.NlpService;
+//import com.stackroute.service.QuestionStorageService;
+//import org.json.simple.JSONObject;
+//import org.json.simple.JSONValue;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//
+//
+//@Service
+//public class KafkaConsumer {
+//    private KafkaProducer kafkaProducer;
+//    private NlpService nlpService;
+//    private QuestionStorageService questionStorageService;
+//
+//    @Autowired
+//    public KafkaConsumer(NlpService nlpService,
+//                         KafkaProducer kafkaProducer,
+//                         QuestionStorageService questionStorageService) {
+//        this.kafkaProducer = kafkaProducer;
+//        this.nlpService = nlpService;
+//        this.questionStorageService = questionStorageService;
+//    }
+//
+//    @KafkaListener(topics = "UpstreamResults", groupId = "group_id")
+//    public void consume(String message) {
+//        JSONObject object = (JSONObject) JSONValue.parse(message);
+//        SearchJSON searchJSON = new SearchJSON(object.get("searchString").toString(), object.get("sessionId").toString());
+//        ArrayList<String> concepts = new ArrayList<>(Arrays.asList("annotations", "ioc container", "beans", "spring core", "spring data jpa", "spring datajpa", "spring aop", "spring security", "spring cloud", "spring reactive", "spring mvc"));
+//        nlpService.setSessonId(searchJSON.getSessonId());
+//        nlpService.setParagraph(searchJSON.getSearchString());
+//        nlpService.setConceptName(concepts);
+//        NlpResult nlpResult = nlpService.getNlpResults();
+//        if (nlpResult.getIntent().equalsIgnoreCase("no intent found")) {
+//            String searchStringQuestion = searchJSON.getSearchString();
+//            questionStorageService.saveQuestion(searchStringQuestion);
+//            kafkaProducer.postservice();
+//        } else {
+//            kafkaProducer.postservice();
+//        }
+//    }
+//}
+//
