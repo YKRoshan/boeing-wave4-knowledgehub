dfb2d2da2bd9b2f0a9defaa787180903c5fb3b96
naneeth <naneethc003@gmail.com>
2019-03-04 00:01:24 +0530

    modified home page

--- angular-frontend/src/app/searchcard/searchcard.component.ts
+++ angular-frontend/src/app/searchcard/searchcard.component.ts
@@ -15,33 +15,14 @@ declare var webkitSpeechRecognition: any;
   templateUrl: './searchcard.component.html',
   styleUrls: ['./searchcard.component.css']
 })
-export class SearchcardComponent  {
-  title :any;
+export class SearchcardComponent {
+  title: any;
   info;
   pageLength: number;
   pageSize: 2;
   pageSizeOptions = [2, 4, 6, 8];
-  splicedData: any; 
-
-
-  ngOnInit() { 
-      
-    this.search.getAll().subscribe((data)=>{
-      this.info=data;
-      this.pageLength=this.info.length;
-      console.log(this.pageLength);
-      this.loadData(0)});
-   
-  }
-
-  loadData(pageIndex){
-    this.splicedData= this.info.slice(pageIndex,pageIndex+2);
-  }
-
-  pageChangeEvent(event) {
-    const offset = ((event.pageIndex + 1) - 1) * event.pageSize;
-    this.splicedData = this.info.slice(offset).slice(0, event.pageSize);
-  }
+  splicedData: any;
+  searchTerm: string;
 
   // viewChild- Property decorator that configures a view query
   // The change detector looks for the first element or the directive matching
@@ -49,32 +30,53 @@ export class SearchcardComponent  {
   // matches the selector, the property is updated.
   @ViewChild('gSearch') formSearch;
   @ViewChild('searchKey') hiddenSearchHandler;
-  searchTerm: string;
   constructor(
     private injector: Injector,
-    private state : TransferState,
-    private search : SearchinfoService,
-  private router: Router,
-    private dataService:DataService ,
+    private state: TransferState,
+    private search: SearchinfoService,
+    private router: Router,
+    private dataService: DataService,
 
-    @Inject(PLATFORM_ID) private platformid: Object,private route:Router
-  ){
+    @Inject(PLATFORM_ID) private platformid: Object, private route: Router
+  ) {
     this.title = 'Voice Search POC';
     //isPlatformServer -Returns whether a platform id represents a server platform.
-    if(isPlatformServer(this.platformid)){
+    if (isPlatformServer(this.platformid)) {
       //this.injector.get - Retrieves an instance from the injector based on the provided token.
       //injector get - abstract get<T>(token: Type<T> | InjectionToken<T>, notFoundValue?: T, flags?: InjectFlags): T
-      const envJson = this.injector.get('CONFIG')? this.injector.get('CONFIG'): {};
+      const envJson = this.injector.get('CONFIG') ? this.injector.get('CONFIG') : {};
       //state of TransferState set method- set<T>(key: StateKey<T>, value: T): void
       this.state.set(configKey, envJson as any);
+    }
   }
-}
+
+  ngOnInit() {
+    this.searchTerm = this.dataService.dataService
+    this.search.getAll().subscribe((data) => {
+      this.info = data;
+      this.pageLength = this.info.length;
+      console.log(this.pageLength);
+      this.loadData(0)
+    });
+    
+
+  }
+
+  loadData(pageIndex) {
+    this.splicedData = this.info.slice(pageIndex, pageIndex + 2);
+  }
+
+  pageChangeEvent(event) {
+    const offset = ((event.pageIndex + 1) - 1) * event.pageSize;
+    this.splicedData = this.info.slice(offset).slice(0, event.pageSize);
+  }
 
 
-public voiceSearch(){
-  //we create the webkitSpeechRecognition object which provides the speech interface,
-  // and set some of its attributes and event handlers.
-  if('webkitSpeechRecognition' in window){
+
+  public voiceSearch() {
+    //we create the webkitSpeechRecognition object which provides the speech interface,
+    // and set some of its attributes and event handlers.
+    if ('webkitSpeechRecognition' in window) {
       const vSearch = new webkitSpeechRecognition();
       vSearch.continuous = false;
       vSearch.interimresults = false;
@@ -83,27 +85,29 @@ public voiceSearch(){
       vSearch.start();
       const voiceSearchForm = this.formSearch.nativeElement;
       const voiceHandler = this.hiddenSearchHandler.nativeElement;
-      vSearch.onresult = function(event){
+      vSearch.onresult = function (event) {
         //storing the result in value
         //returns a string containing the transcript of the recognised word(s).
         voiceHandler.value = event.results[0][0].transcript;
-          vSearch.stop();
-          //submitting the form with the value
-          // voiceSearchForm.submit();
+        vSearch.stop();
+        //submitting the form with the value
+        // voiceSearchForm.submit();
       }
       //if error is encountered, show error in console and stop the speech recognition
-      vSearch.onerror = function(event){
-          console.log(event);
-          vSearch.stop();
+      vSearch.onerror = function (event) {
+        console.log(event);
+        vSearch.stop();
       }
-  } 
-  //if browser does not have webkitspeechrecognition
-  else {
-    console.log("Your browser does not support voice recognition feature.");
     }
-}
-
-
-
+    //if browser does not have webkitspeechrecognition
+    else {
+      console.log("Your browser does not support voice recognition feature.");
+    }
+  }
+    
+  getdata() {  
+    this.dataService.dataService = this.searchTerm; 
+    this.router.navigate(['/cards']);
+  }
 
 }
