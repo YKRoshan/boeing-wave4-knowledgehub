577925cbc07ccc6b4df5cf2c24e9268c156f04ea
urvashi.ratna <urvashi.ratna@boeing.com>
2019-03-11 15:21:51 +0530

    Added Admin UI front end

--- angular-frontend/src/app/admin-ui/admin-ui.component.ts
+++ angular-frontend/src/app/admin-ui/admin-ui.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-admin-ui',
   templateUrl: './admin-ui.component.html',
   styleUrls: ['./admin-ui.component.css']
 })
 export class AdminUiComponent implements OnInit {
+  domain:string;
+  concept:string;
+  conceptArray : String[];
 
   constructor() { }
 
   ngOnInit() {
   }
+  display(){
+  
+    this.conceptArray=this.concept.split(",");
+    console.log(this.domain);
+    console.log(this.concept);
+    console.log(this.conceptArray);
 
+  }
 }
