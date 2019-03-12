56b04d074d4afd3dd720f41b4191b8db8d9034cd
gauravgkp <gkpgauravsingh@gmail.com>
2019-03-12 09:28:13 +0530

    updated file websocketService application with comment in it.

--- websocket-service/src/main/java/com/stackroute/WebsocketServiceApplication.java
+++ websocket-service/src/main/java/com/stackroute/WebsocketServiceApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebsocketServiceApplication {
 
+	/*
+	This is web socket main class which runsthe web socket service applications.
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketServiceApplication.class, args);
 	}
