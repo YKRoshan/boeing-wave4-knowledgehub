2554f45f40f661f68c31679a83c1486b062e0467
Komal_R <komalr984@gmail.com>
2019-03-14 12:02:50 +0530

    converting properties to yml

--- angular-frontend/src/app/admin-ui/admin-ui.component.ts
+++ angular-frontend/src/app/admin-ui/admin-ui.component.ts
@@ -22,11 +22,11 @@ export class AdminUiComponent implements OnInit {
     console.log(this.domain);
     console.log(this.concept);
     console.log(this.conceptArray);
-    this.http.post("http://localhost:8080/domain",{
+    this.http.post("http://localhost:8099/domain",{
       "domain":this.domain,
       "conceptName":this.conceptArray
     }).subscribe((data)=>{
       console.log(data);
-    })
+    })  
   }
 }
